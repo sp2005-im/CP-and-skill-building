f280a3a74460a55ccf43403157316467142db815
Srihari Prasad <sp2005im@gmail.com>
2025-06-09 08:45:40 +0530

    Create Frog_2.cpp

---
+++ AtCoder Contest Problems /Beginner Contest/Educational DP Contest/Frog_2.cpp
@@ -0,0 +1,32 @@
+//Sri Rama Jayam
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<climits>
+
+using namespace std;
+
+int get_min_cost(vector<int>steps, int n, int k){
+        vector<int>dp(n, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i < n; ++i){
+                for (int j = 1; j <= k; ++j){
+                        int cost = abs(steps[i]-steps[i-j]);
+                        dp[i] = min(dp[i], dp[i-j]+cost);
+                }
+        }
+        return dp[n-1];
+}
+
+int main(){
+        int n, k;
+        cin>>n>>k;
+        vector<int>steps(n);
+        for (int i = 0; i < n; ++i){
+                cin>>steps[i];
+        }
+        int cost = get_min_cost(steps,n,k);
+        cout<<cost<<endl;
+        return 0;
+}
